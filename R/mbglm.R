f8c1c1d220036334f0d6c967ad322bcb5f529505
nyiuab <nyi@uab.edu>
2020-02-08 18:47:08 -0600

    Add files via upload

--- R/mbglm.R
+++ R/mbglm.R
@@ -1,14 +1,12 @@
 
 
 mbglm <- function(y, formula, data, family=NegBin(), prior=Student(0, 1),
-                  verbose=TRUE)
+                  min.p=0, verbose=TRUE)
 {
   start.time <- Sys.time()
   
   call <- match.call()
-  y <- as(y, "matrix")
-  if (NCOL(y)==1) stop("'y' should include multiple columns (responses)")
-  if (is.null(colnames(y))) colnames(y) <- paste("y", 1:ncol(y), sep="")
+  y <- nonzero(y=y, min.p=min.p, sort=FALSE)$y.filter
   if (missing(data)) data <- NULL
   
   if (verbose) cat("Analyzing", ncol(y), "responses: \n")
@@ -40,7 +38,7 @@ mbglm <- function(y, formula, data, family=NegBin(), prior=Student(0, 1),
   res
 }
 
-summary.mbglm <- function(object, vr.name=NULL) 
+summary.mbglm <- function(object, vr.name=NULL, sort=FALSE) 
 {
   obj.fit <- object$fit
   obj.resp <- object$responses
@@ -68,11 +66,42 @@ summary.mbglm <- function(object, vr.name=NULL)
   if (!is.null(vr.name))
   {
     if (!vr.name %in% c(obj.resp, obj.vars)) stop("wrong name given")
-    if (vr.name %in% obj.resp) res0 <- res[res[, 1]==vr.name, ]
-    if (vr.name %in% obj.vars) res0 <- res[res[, 2]==vr.name, ]
+    if (vr.name %in% obj.resp) {
+      res0 <- res[res[, 1]==vr.name, ]
+      rownames(res0) <- res0[, 2]  
+    }
+    if (vr.name %in% obj.vars) {
+      res0 <- res[res[, 2]==vr.name, ]
+      rownames(res0) <- res0[, 1]
+    }
+    res0 <- as.matrix(res0[, 3:6])
+    if (sort) res0 <- res0[names(sort(res0[, "padj"])), ]
     out <- list(res=res, res0=res0)
   }
   
   return(out)
 }
+
+nonzero <- function(y, total, min.p=0, sort=TRUE, plot=FALSE)
+{
+  y <- as(y, "matrix")
+  if (is.null(colnames(y))) colnames(y) <- paste("y", 1:ncol(y), sep = "")
+  if(missing(total)) total <- rowSums(y)
+  total <- unlist(total)
+  nonzero.p <- apply(y, 2, function(z) {length(z[z != 0])/length(z)} )
+  if (sort) nonzero.p <- sort(nonzero.p, decreasing=T)
+  sub <- names(nonzero.p[nonzero.p > min.p])
+  if (length(sub) == 0) stop("min.p is too large") 
+  else y0 <- y[, sub, drop=FALSE]
+  total.mean <- round(mean(total), 2)
+  total.sd <- round(sd(total), 2)
+  if (plot){
+    par(mfrow = c(1, 2), mar = c(5, 4, 4, 4))
+    hist(total, nclass=1000, xlab="Total reads", main="")
+    zero <- sort(1 - nonzero.p, decreasing=F)
+    plot(x=1:length(zero), y=zero, xlab="Taxa", ylab="Zero Proportion")
+  }
+  list(nonzero.p=nonzero.p, total=total, total_mean_sd=c(total.mean, total.sd), y.filter=y0)
+}
+
 
